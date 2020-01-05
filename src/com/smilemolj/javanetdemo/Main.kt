487998c68375a36f31a388b133e5fe62a2299e41
smilemolj <smilemolj@gmail.com>
2020-01-05 11:11:35 +0800

    perfect

--- src/com/smilemolj/javanetdemo/Main.kt
+++ src/com/smilemolj/javanetdemo/Main.kt
@@ -1,8 +1,7 @@
-package com.smilemolj.javanetdemo;
+package com.smilemolj.javanetdemo
 
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) { // write your code here
     }
 }
