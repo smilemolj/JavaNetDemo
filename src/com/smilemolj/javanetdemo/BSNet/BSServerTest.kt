0c1319021bf837af07ae8729b16f6300af5eb5b4
smilemolj <smilemolj@gmail.com>
2020-01-05 21:32:04 +0800

    perfect

--- src/com/smilemolj/javanetdemo/BSNet/BSServerTest.kt
+++ src/com/smilemolj/javanetdemo/BSNet/BSServerTest.kt
@@ -1,10 +1,25 @@
 package com.smilemolj.javanetdemo.BSNet
 
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class BSServerTest {
     @Test
-    fun testAdd(){
-        println("666")
+    fun testAdd() {
+        println("testAdd")
+        val i = 666
+//        assertEquals(3,i)
+        assert(i==666)
+    }
+
+    @Before
+    fun init(){
+        println("init")
+    }
+
+    @After
+    fun close(){
+        println("close")
     }
 }
