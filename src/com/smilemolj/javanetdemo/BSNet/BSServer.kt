9a3f9af5e851e6aa17e24b401923723cbb59b670
smilemolj <smilemolj@gmail.com>
2020-01-05 16:33:25 +0800

    perfect

--- src/com/smilemolj/javanetdemo/BSNet/BSServer.kt
+++ src/com/smilemolj/javanetdemo/BSNet/BSServer.kt
@@ -5,36 +5,40 @@ import java.io.File
 import java.io.FileInputStream
 import java.io.InputStreamReader
 import java.net.ServerSocket
+import kotlin.concurrent.thread
 
 object BSServer {
     @JvmStatic
     fun main(args: Array<String>) {
         val server = ServerSocket(8080)
-        val socket = server.accept()
-        val inputStream = socket.getInputStream()
+        while (true) {
+            val socket = server.accept()
+            thread {
+                val inputStream = socket.getInputStream()
 //        val bytes = ByteArray(1024)
 //        var len = 0
 //        while (inputStream.read(bytes).let { len = it;it != -1 }) {
 //            println(String(bytes, 0, len))
 //        }
-        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-        val readLine = bufferedReader.readLine()
-        val split = readLine.split(" ")
-        val htmlpath = split[1].substring(1)
-        println(File(htmlpath).absolutePath)
-        //http://127.0.0.1:8080/web/index.html
-        val fileInputStream = FileInputStream(htmlpath)
-        val outputStream = socket.getOutputStream()
-        outputStream.write("HTTP/1.1 200 OK\r\n".toByteArray())
-        outputStream.write("Content-Type:text/html\r\n".toByteArray())
-        outputStream.write("\r\n".toByteArray())
-        var len = 0
-        val bytes = ByteArray(1024)
-        while (fileInputStream.read(bytes).let { len = it;it != -1 }) {
-            outputStream.write(bytes, 0, len)
+                val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+                val readLine = bufferedReader.readLine()
+                val split = readLine.split(" ")
+                val htmlpath = split[1].substring(1)
+                println(File(htmlpath).absolutePath)
+                //http://127.0.0.1:8080/web/index.html
+                val fileInputStream = FileInputStream(htmlpath)
+                val outputStream = socket.getOutputStream()
+                outputStream.write("HTTP/1.1 200 OK\r\n".toByteArray())
+                outputStream.write("Content-Type:text/html\r\n".toByteArray())
+                outputStream.write("\r\n".toByteArray())
+                var len = 0
+                val bytes = ByteArray(1024)
+                while (fileInputStream.read(bytes).let { len = it;it != -1 }) {
+                    outputStream.write(bytes, 0, len)
+                }
+                fileInputStream.close()
+                socket.close()
+            }
         }
-        fileInputStream.close()
-        socket.close()
-        server.close()
     }
 }
