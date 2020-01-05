62f6f7345cca49045ee1b485879b5f53fedb553a
smilemolj <smilemolj@gmail.com>
2020-01-05 14:49:41 +0800

    perfect

--- src/com/smilemolj/javanetdemo/TCPClient.kt
+++ src/com/smilemolj/javanetdemo/TCPClient.kt
@@ -1,13 +1,36 @@
 package com.smilemolj.javanetdemo
 
+import java.io.FileInputStream
 import java.net.Socket
 
 object TCPClient {
     @JvmStatic
     fun main(args: Array<String>) { // write your code here
-        val socket=Socket("127.0.0.1",8888)
-        val os = socket.getOutputStream()
-        os.write("你好服务器".toByteArray())
+//        val socket = Socket("127.0.0.1", 8888)
+//        val os = socket.getOutputStream()
+//        os.write("你好服务器".toByteArray())
+//        val inputStream = socket.getInputStream()
+//        val bytes = ByteArray(1024)
+//        val len = inputStream.read(bytes)
+//        println(String(bytes, 0, len))
+//        socket.close()
+
+        val fileInputStream = FileInputStream("/Users/lijunliang/Desktop/66.jpg")
+        val socket = Socket("127.0.0.1", 8888)
+        val outputStream = socket.getOutputStream()
+        val bytes = ByteArray(1024)
+        var len = 0
+        while (fileInputStream.read(bytes).let { len = it;it != -1 }) {
+            outputStream.write(bytes, 0, len)
+        }
+        socket.shutdownOutput()
+
+        val inputStream = socket.getInputStream()
+        while (inputStream.read(bytes).let { len = it;it != -1 }) {
+            println(String(bytes, 0, len))
+        }
+
+        fileInputStream.close()
         socket.close()
     }
 }
--- src/com/smilemolj/javanetdemo/TCPServer.kt
+++ src/com/smilemolj/javanetdemo/TCPServer.kt
@@ -1,19 +1,43 @@
 package com.smilemolj.javanetdemo
 
+import java.io.File
+import java.io.FileOutputStream
 import java.net.ServerSocket
+import java.util.*
 
 object TCPServer {
     @JvmStatic
     fun main(args: Array<String>) { // write your code here
+//        val server = ServerSocket(8888)
+//        val socket = server.accept()
+//        val inputStream = socket.getInputStream()
+//        val bytes = ByteArray(1024)
+//        val len = inputStream.read(bytes)
+//        println(String(bytes, 0, len))
+//        val outputStream = socket.getOutputStream()
+//        outputStream.write("收到，谢谢啦".toByteArray())
+//        socket.close()
+//        server.close()
         val server = ServerSocket(8888)
-        val socket = server.accept()
-        val inputStream = socket.getInputStream()
-        val bytes = ByteArray(1024)
-        val len = inputStream.read(bytes)
-        println(String(bytes, 0, len))
-        val outputStream = socket.getOutputStream()
-        outputStream.write("收到，谢谢啦".toByteArray())
-        socket.close()
-        server.close()
+        while (true) {
+            val socket = server.accept()
+            val inputStream = socket.getInputStream()
+            val file = File("/Users/lijunliang/Desktop/server")
+            if (!file.exists()) {
+                file.mkdirs()
+            }
+            val filename = "demo" + System.currentTimeMillis() + Random().nextInt(999999) + ".jpg"
+            val fileOutputStream = FileOutputStream("$file/$filename")
+            val bytes = ByteArray(1024)
+            var len = 0
+            while (inputStream.read(bytes).let { len = it;it != -1 }) {
+                fileOutputStream.write(bytes, 0, len)
+            }
+
+            val outputStream = socket.getOutputStream()
+            outputStream.write("上传成功啦".toByteArray())
+            fileOutputStream.close()
+            socket.close()
+        }
     }
 }
