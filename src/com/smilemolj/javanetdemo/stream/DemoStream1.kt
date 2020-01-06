5bffe91355153d13fcb29a1a67301bb670d695e9
smilemolj <smilemolj@gmail.com>
2020-01-06 12:51:46 +0800

    perfect

---
+++ src/com/smilemolj/javanetdemo/stream/DemoStream1.kt
@@ -0,0 +1,8 @@
+package com.smilemolj.javanetdemo.stream
+
+object DemoStream1 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+    }
+}
--- src/com/smilemolj/javanetdemo/stream/DemotoStream.kt
+++ src/com/smilemolj/javanetdemo/stream/DemotoStream.kt
@@ -7,17 +7,32 @@ import java.util.stream.Stream
 object DemotoStream {
     @JvmStatic
     fun main(args: Array<String>) {
-        val list: MutableList<String> = ArrayList()
-        val stream = list.stream()
+//        val list: MutableList<String> = ArrayList()
+//        val stream = list.stream()
+//
+//        val hashSet = HashSet<String>()
+//        val stream1 = hashSet.stream()
+//
+//        val hashMap = HashMap<String, String>()
+//        val keys = hashMap.keys
+//        val stream2 = keys.stream()
+//
+//        val of = Stream.of(1, 2, 3, 4, 5, 6)
 
-        val hashSet = HashSet<String>()
-        val stream1 = hashSet.stream()
+        val stream = Stream.of("张三", "李四", "王武", "赵六", "田七")
+        val filter = stream.filter {
+            it.startsWith("张")
+        }
+        filter.forEach {
+            println(it)
+        }
 
-        val hashMap = HashMap<String, String>()
-        val keys = hashMap.keys
-        val stream2 = keys.stream()
-
-        val of = Stream.of(1, 2, 3, 4, 5, 6)
-
+        val stream1 = Stream.of("1", "2", "3", "4", "5")
+        val map = stream1.map {
+            it.toInt()
+        }
+        map.forEach {
+            println(it)
+        }
     }
 }
